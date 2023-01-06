3c6bc552e6bb6eb6d758507a0346360abf9d8e02
Jeffry Alexander Acosta Angulo <jeffryaaads@hotmail.com>
2023-01-06 17:35:07 -0600

    Agregando componentes de material y paquetes (moment y moment-adapter)
    agregando componentes de material
    agregando paquetes @angular/material-moment-adapter
    agregando paquete moment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCommonModule } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+
+// Componentes
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +25,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
